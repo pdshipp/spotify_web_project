912465116549de0f1d6229b828469b8cff16f879
pdshipp <peterdshipp@gmail.com>
2020-08-11 09:21:51 +0100

    change to header style

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Title </h1>
+        <h1 style = {{...defaultStyle, 'font-size': '60px' }}> Title </h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
