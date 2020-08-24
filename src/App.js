cac1e2588688a62731dccc82a6ac13b79917f834
pdshipp <peterdshipp@gmail.com>
2020-08-24 18:21:54 +0100

    added server data to playlist component

--- src/App.js
+++ src/App.js
@@ -94,11 +94,16 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render() {
+    let playlist = this.props.playlist
     return (
       <div style = {{... defaultStyle, width: '20%', display: 'inline-block'}}>
         <img/>
-        <h3> Playlist Name </h3>
-        <ul> <li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+        <h3>{playlist.name}</h3>
+        <ul>
+          {playlist.songs.map(song =>
+            <li>{song.name}</li>
+          )}
+        </ul>
       </div>
     );
   }
@@ -138,10 +143,9 @@ class App extends Component {
           <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
           <HoursCounter playlists={this.state.serverData.user.playlists}/>
           <Filter/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
+          {this.state.serverData.user.playlists.map(
+            playlist => <Playlist playlist={playlist}/>
+          )}
         </div> : <h1> 'Loading...' </h1>
         }
       </Wrapper>
