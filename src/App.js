58f03f47ae8bc5d4425fbac1f38889c364dffe9d
pdshipp <peterdshipp@gmail.com>
2020-09-06 12:34:17 +0100

    fetch my profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import styled, {css} from 'styled-components';
+import React, { Component } from "react";
+import "./App.css";
+import styled, { css } from "styled-components";
+import queryString from "query-string";
 
 const defaultStyle = {
-  color: 'black'
+  color: "black",
 };
 
 const textStyle = css`
@@ -12,51 +13,52 @@ const textStyle = css`
 
 let fakeServerData = {
   user: {
-    name: 'Pete',
+    name: "Pete",
     playlists: [
       {
-        name: 'Canapes',
+        name: "Canapes",
         songs: [
-          {name: 'Lost in Paris', duration: 1234},
-          {name: 'Water Baby', duration: 3456},
-          {name: 'Heartbreaker', duration: 6789}
-        ]
+          { name: "Lost in Paris", duration: 1234 },
+          { name: "Water Baby", duration: 3456 },
+          { name: "Heartbreaker", duration: 6789 },
+        ],
       },
       {
-        name: 'R&B',
+        name: "R&B",
         songs: [
-          {name: 'Do For Love', duration: 1234},
-          {name: 'Best Friend', duration: 3456},
-          {name: 'Just a Lil Bit', duration: 6789}
-        ]
+          { name: "Do For Love", duration: 1234 },
+          { name: "Best Friend", duration: 3456 },
+          { name: "Just a Lil Bit", duration: 6789 },
+        ],
       },
       {
-        name: 'Runnin\'',
+        name: "Runnin'",
         songs: [
-          {name: 'No One Gets You', duration: 1234},
-          {name: 'Free Myself', duration: 3456},
-          {name: 'The Drive', duration: 6789}
-        ]
+          { name: "No One Gets You", duration: 1234 },
+          { name: "Free Myself", duration: 3456 },
+          { name: "The Drive", duration: 6789 },
+        ],
       },
       {
-        name: 'Dance',
+        name: "Dance",
         songs: [
-        {name: '7 Days', duration: 1234},
-        {name: 'Believe', duration: 3456},
-        {name: 'The Bomb', duration: 6789}
-        ]
+          { name: "7 Days", duration: 1234 },
+          { name: "Believe", duration: 3456 },
+          { name: "The Bomb", duration: 6789 },
+        ],
       },
-    ]
-  }
-}
-
-
+    ],
+  },
+};
 
 class PlaylistCounter extends Component {
   render() {
     return (
-      <div style = {{... defaultStyle, width: '20%',display: 'inline-block'}}>
-        <h2> {this.props.playlists && this.props.playlists.length} Playlists </h2>
+      <div style={{ ...defaultStyle, width: "20%", display: "inline-block" }}>
+        <h2>
+          {" "}
+          {this.props.playlists && this.props.playlists.length} Playlists{" "}
+        </h2>
       </div>
     );
   }
@@ -64,46 +66,46 @@ class PlaylistCounter extends Component {
 
 class HoursCounter extends Component {
   render() {
-    let allSongs = this.props.playlists.reduce((songs, eachPlaylist)=> {
-      return songs.concat(eachPlaylist.songs)
-    },[]);
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs);
+    }, []);
     let totalDuration = allSongs.reduce(
-      (sum, eachSong) => sum + eachSong.duration
-      , 0);
+      (sum, eachSong) => sum + eachSong.duration,
+      0
+    );
     return (
-      <div style = {{... defaultStyle, width: '20%',display: 'inline-block'}}>
-        <h2> {Math.round(totalDuration/60)} Hours </h2>
+      <div style={{ ...defaultStyle, width: "20%", display: "inline-block" }}>
+        <h2> {Math.round(totalDuration / 60)} Hours </h2>
       </div>
     );
   }
 }
 
-
-
 class Filter extends Component {
   render() {
     return (
-      <div style = {defaultStyle}>
-        <img/>
-        <input type="text" onKeyUp={event =>
-          this.props.onTextChange(event.target.value)}/>
+      <div style={defaultStyle}>
+        <img />
+        <input
+          type="text"
+          onKeyUp={(event) => this.props.onTextChange(event.target.value)}
+        />
       </div>
-      )
+    );
   }
 }
 
-
 class Playlist extends Component {
   render() {
-    let playlist = this.props.playlist
+    let playlist = this.props.playlist;
     return (
-      <div style = {{... defaultStyle, width: '20%', display: 'inline-block'}}>
-        <img/>
+      <div style={{ ...defaultStyle, width: "20%", display: "inline-block" }}>
+        <img />
         <h3>{playlist.name}</h3>
         <ul>
-          {playlist.songs.map(song =>
+          {playlist.songs.map((song) => (
             <li>{song.name}</li>
-          )}
+          ))}
         </ul>
       </div>
     );
@@ -117,47 +119,57 @@ const Wrapper = styled.div`
 const H1 = styled.h1`
   ${textStyle};
   font-size: 60px;
-  color: ${(props => props.color)};
+  color: ${(props) => props.color};
 `;
 
-
 class App extends Component {
-  state = {serverData : {},
-  filterString: ''
-  }
-// mentioned getting rid of constructor - how do we do this?
+  state = { serverData: {}, filterString: "" };
   componentDidMount() {
-    setTimeout(()=> {
-      this.setState({serverData: fakeServerData});
-    }, 1000);
-  }
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
 
+    fetch("https://api.spotify.com/v1/me", {
+      headers: { Authorization: "Bearer " + accessToken },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
   render() {
-    let playlistsToRender = this.state.serverData.user ? this.state.serverData.user.playlists
-      .filter(playlist =>
-        playlist.name.toLowerCase().includes(
-          this.state.filterString.toLowerCase())
-    ) : []
+    let playlistsToRender = this.state.serverData.user
+      ? this.state.serverData.user.playlists.filter((playlist) =>
+          playlist.name
+            .toLowerCase()
+            .includes(this.state.filterString.toLowerCase())
+        )
+      : [];
     return (
       <Wrapper>
         <H1> Playlist Generator </H1>
-        {
-          this.state.serverData.user ?
+        {this.state.serverData.user ? (
           <div>
-            <h2 style = {{...defaultStyle, 'font-size': '40px'}}>
-            Welcome, {this.state.serverData.user.name}.
-          </h2>
-          <PlaylistCounter playlists={playlistsToRender}/>
-          <HoursCounter playlists={playlistsToRender}/>
-          <Filter onTextChange={text => this.setState({filterString: text})}/>
-          {playlistsToRender.map(
-            playlist => <Playlist playlist={playlist}/>
-          )}
-        </div> : <h1> 'Loading...' </h1>
-        }
+            <h2 style={{ ...defaultStyle, "font-size": "40px" }}>
+              Welcome, {this.state.serverData.user.name}.
+            </h2>
+            <PlaylistCounter playlists={playlistsToRender} />
+            <HoursCounter playlists={playlistsToRender} />
+            <Filter
+              onTextChange={(text) => this.setState({ filterString: text })}
+            />
+            {playlistsToRender.map((playlist) => (
+              <Playlist playlist={playlist} />
+            ))}
+          </div>
+        ) : (
+          <button
+            onClick={() => (window.location = "http://localhost:8888/login")}
+            style={{ padding: "20px", fontSize: "20px" }}>
+            {" "}
+            Sign in with Spotify{" "}
+          </button>
+        )}
       </Wrapper>
     );
   }
 }
 
-  export default App;
+export default App;
